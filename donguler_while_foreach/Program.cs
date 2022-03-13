a71ecd01aeb31bb7233aea82d63671929425afe4
Emre Can <emrecanoner@eskisehir.edu.tr>
2022-03-13 17:05:26 +0300

    new application

---
+++ donguler_while_foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //1 ile kullanıcıdan alınan sayıya kadar olan sayıların ortalamasını bul.
+            Console.Write("Sayı giriniz: ");
+            int num = int.Parse(Console.ReadLine());
+            int counter=1, total=0;
+            while(counter<=num)
+            {
+                total+=counter;
+                counter++;
+            }
+            Console.WriteLine("Ortalama: "+ total/num);
+
+            //'a' dan 'z' ye kadar tüm harfleri yazdır.
+            char c = 'a';
+            while (c<='z')
+            {  
+                Console.WriteLine(c);
+                c++;    
+            }
+
+            //ForEach
+            Console.WriteLine("***** ForEach *****");
+            string[] cars = {"BMW", "Toyota", "Nissan", "Chevrolet"};
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
