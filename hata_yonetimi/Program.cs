b8ce2cfb62e97d790a9b0cd4ae95153f1c4bd963
Emre Can <emrecanoner@eskisehir.edu.tr>
2022-03-13 15:26:42 +0300

    new application

---
+++ hata_yonetimi/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Bir sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girdiğiniz sayı: "+ sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata: "+ ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Çok büyük ya da çok küçük bir değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
